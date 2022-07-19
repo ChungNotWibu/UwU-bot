e12d789401a1d2983de541efec91f05dff325e5d
ChungNotWibu <h27102006@gmail.com>
2022-07-19 17:32:19 +0700

    m

--- ext/command.py
+++ ext/command.py
@@ -1,5 +1,10 @@
 
+from ast import parse
+from datetime import datetime
+import re
 from turtle import title
+from urllib.request import Request
+from aiohttp import request
 import discord
 from discord.ext import commands
 
@@ -49,16 +54,6 @@ class Command(commands.Cog):
         except:
             await ctx.send(f'Không có tin nhắn đã xóa trong #{channel.name}')    
 
-    #userinfo
-    @commands.command()
-    async def info(ctx):
-        embed = discord.Embed(title=f"{ctx.guild.name}", description="Lorem Ipsum asdasd", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
-        embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
-        embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
-        embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
-        embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
-        embed.set_thumbnail(url=f"{ctx.guild.icon}")
-        await ctx.send(embed=embed)
 
 
 
