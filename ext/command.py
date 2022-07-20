7c84e3bd0ae68ccd4acb6a066f2ebdb6e6a127e7
ChungNotWibu <h27102006@gmail.com>
2022-07-20 18:12:19 +0700

    acv

--- ext/command.py
+++ ext/command.py
@@ -17,6 +17,7 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 from hikari import Guild
+# ===========================================================
 
 # ==========snipe===========
 snipe_message_author = {}
