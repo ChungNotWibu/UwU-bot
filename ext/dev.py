88ba666dddb2f5a600b5f0ed1b3032042b90eaa9
ChungNotWibu <h27102006@gmail.com>
2022-07-28 22:25:18 +0700

    aaa

---
+++ ext/dev.py
@@ -0,0 +1,43 @@
+# from multiprocessing.spawn import import_main_path
+# from pydoc import text
+# from turtle import title
+# from typing import Text
+
+
+# import discord
+# from discord.ext import commands
+
+
+# # ===========================================================
+
+
+# class Command(commands.Cog):
+
+#     def __init__(self, client):
+#         self.client = client
+
+#     # kick
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def setup(client):
+#     client.add_cog(Command(client))
