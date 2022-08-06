62f4b6854d60c71accabddd5fa5c620ed3fd5550
ChungNotWibu <h27102006@gmail.com>
2022-08-06 23:18:48 +0700

    aaa

--- ext/general.py
+++ ext/general.py
@@ -70,11 +70,9 @@ class Command(commands.Cog):
         embed.set_author(name=f"User Info - {user}"),
         embed.set_thumbnail(url=user.avatar_url),
         embed.set_footer(text=f'Requested by - {ctx.author}',icon_url=ctx.author.avatar_url)
-        embed.add_field(name='ID:', value=user.id, inline=False)
         embed.add_field(name='Name:', value=user.display_name, inline=False)
         embed.add_field(name='Created at:',value=user.created_at, inline=False)
         embed.add_field(name='Joined at:', value=user.joined_at, inline=False)
-        embed.add_field(name='Bot?', value=user.bot, inline=False)
         embed.add_field(name=f'Roles:({len(rlist)})',value=''.join([b]), inline=False)
         embed.add_field(name='Top Role:',value=user.top_role.mention, inline=False)
         await ctx.send(embed=embed)
